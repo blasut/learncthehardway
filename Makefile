760eff8db67998a94b6a01ea233ce282925144c8
oklaridentitet <oklaridentitet@gmail.com>
2014-10-14 12:05:24 +0200

    add ex3 and ex4

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 CFLAGS=-Wall -g
 
 all:
-	gcc ex1.c
+	gcc ex1.c -o ex1.out
+	gcc ex3.c -o ex3.out
+	gcc ex4.c -o ex4.out
 
 clean:
-	rm -f a.out
+	rm -f ex1.out
+	rm -f ex3.out
+	rm -f ex4.out
---
+++ ex4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Warning: This program is wrong on purpose. */
+
+int main()
+{
+    int age = 10;
+    int heigth = 70;
+    
+    printf("I am %d years old. \n", age);
+    printf("I am %d inches tall.\n", heigth);
+    
+    return 0;
+    
+}
