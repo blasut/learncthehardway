078e30330268d4906c5c866a09e8748a564bbc95
blasut <blasut937@gmail.com>
2016-05-14 18:32:57 +0200

    ex22

---
+++ ex22/ex22.h
@@ -0,0 +1,14 @@
+#ifndef _ex22_h
+#define _ex22_h
+
+// makes THE_SIZE in ex22.c available to other .c files
+extern int THE_SIZE;
+
+int get_age();
+void set_age(int age);
+
+double update_ratio(double ratio);
+
+void print_size();
+
+#endif
